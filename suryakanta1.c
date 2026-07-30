1198882f533b7a5b12e2dc1577e4c73523a9b520
Suryakanta Sahoo <sahoosuryakanta44975@GMAIL.COM>
2026-07-29 17:25:26 -0700

    Updated code

--- suryakanta1.c
+++ suryakanta1.c
@@ -2,5 +2,6 @@
       int main(){
       printf(" helo world ");
       return 0;
-      }
+      } 
+
 
--- suryakanta2.c
+++ suryakanta2.c
@@ -4,4 +4,4 @@
          scanf("%d%d",&a,&b);
          printf("%d",a+b);
     return 0;
-     }
+     } 
